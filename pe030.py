53faed4617f0007d24ced16851feafc45334a333
Kotas <kotas3952@gmail.com>
2016-09-30 13:59:31 -0400

    Problems 30 and 31

---
+++ pe030.py
@@ -0,0 +1,45 @@
+"""
+Digit fifth powers
+Problem 30
+
+Surprisingly there are only three numbers that can be written as the sum of
+fourth powers of their digits:
+
+    1634 = 1^4 + 6^4 + 3^4 + 4^4
+    8208 = 8^4 + 2^4 + 0^4 + 8^4
+    9474 = 9^4 + 4^4 + 7^4 + 4^4
+
+As 1 = 14 is not a sum it is not included.
+
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+Find the sum of all the numbers that can be written as the sum of fifth powers
+of their digits.
+"""
+
+def sumOfFifthPowers(n):
+    """
+    Determines in n can be written as the sum of the fifth powers of its digits.
+    """
+    # Must have multiple digits
+    if n < 10:
+        return False
+
+    total = 0
+    for digit in str(n):
+        total += int(digit)**5
+
+    return total == n
+
+def answer():
+    total = 0
+    # Highest possible sum for 7 digits -> 7*9^5 = 413343 which isn't 7 digits
+    # Therefore all values are less than the max of 6 digits
+    for i in xrange(10, 6*9**5):
+        if sumOfFifthPowers(i):
+            total += i
+
+    return total
+
+if __name__ == "__main__":
+    print answer()
---
+++ pe031.py
@@ -0,0 +1,36 @@
+"""
+Coin sums
+Problem 31
+
+In England the currency is made up of pound, and pence, p, and there are
+eight coins in general circulation:
+
+    1p, 2p, 5p, 10p, 20p, 50p, 100p and 200p.
+
+It is possible to make 200p in the following way:
+
+    1*100p + 1*50p + 2*20p + 1*5p + 1*2p + 3*1p
+
+How many different ways can 200p be made using any number of coins?
+"""
+
+def answer():
+    coins = [200, 100, 50, 20, 10, 5, 2, 1]
+    return recurse(200, coins)
+
+def recurse(money, coins):
+    # IF we get to 0p, that means its a valid partition
+    if money == 0:
+        return 1
+
+    # If there are no coins left to add or the money is negative, that means
+    # not a valid partition
+    if len(coins) == 0 or money < 0:
+        return 0
+
+    # Either we can add the coin and deduct the money left or we can skip this
+    # coin. Add these two possiblities.
+    return recurse(money - coins[0], coins) + recurse(money, coins[1:])
+
+if __name__ == "__main__":
+    print answer()
